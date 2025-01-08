cedd8c9823008b2796b5cf6e043042dea3510158
Athina <athina.kantis@gmail.com>
2025-01-08 15:20:36 +0200

    add function to format timeWorked

--- src/utils/calc.js
+++ src/utils/calc.js
@@ -1,3 +1,16 @@
-export function calcListPages(num) {
-    return Math.ceil(num / 8);
-}
+import useEmployeeStatus from './useEmployeeStatus';
+
+export const formatTimeWorked = (startdate) => {
+    const { yearsWorked, monthsWorked, daysWorked } =
+        useEmployeeStatus(startdate);
+
+    if (yearsWorked < 1) {
+        if (monthsWorked < 1) {
+            return daysWorked <= 1 ? '1 day' : `${daysWorked} days`;
+        } else {
+            return `${monthsWorked} months`;
+        }
+    } else {
+        return yearsWorked === 1 ? '1 year' : `${yearsWorked} years`;
+    }
+};

6de848101d0aaf6ff80c63c74f3081bbd23131ed
Athina <athina.kantis@gmail.com>
2025-01-08 09:36:08 +0200

    update Spinner: add version with white background + aliceblue background

--- src/Components/Spinner/Spinner.jsx
+++ src/Components/Spinner/Spinner.jsx
@@ -1,10 +1,14 @@
-function Spinner() {
-    return (
-        <img
-            src='/sf_spinner.gif'
-            alt='Loading...'
-        />
-    );
+function Spinner({ background = 'light' }) {
+  return (
+    <img
+      src={
+        background === 'light'
+          ? `/src/assets/Spinner_white.gif`
+          : `/src/assets/Spinner_aliceblue.gif`
+      }
+      alt='Loading...'
+    />
+  );
 }
 
 export default Spinner;
